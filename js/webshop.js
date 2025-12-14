7150f100ea7f376ddfbe90af86c165514d8e835f
Louise <lodo0001@stud.kea.dk>
2025-12-14 16:25:51 +0100

    færdig_productview

--- js/webshop.js
+++ js/webshop.js
@@ -24,20 +24,58 @@ fetch(endpoint, options)
   .then((response) => response.json())
   .then((data) => showProducts(data))
   .catch((error) => console.error("Fejl:", error));
-
 function showProducts(products) {
   productlistContainer.innerHTML = "";
 
   products.forEach((product) => {
     productlistContainer.innerHTML += `
-      <a href="productview.html?id=${product.id}" class="product_link">
-        <div class="product">
-          <img src="${product.image_url}" alt="${product.name}" class="product_img">
+        <div class="product"
+             data-name="${product.name}"
+             data-price="${product.price}"
+             data-image="${product.image_url}"
+             data-description="${product.description || ""}">
+          <img src="${product.image_url}" alt="${
+      product.name
+    }" class="product_img">
           <h3 class="product_name">${product.name}</h3>
           <p class="product_price">${product.price} kr</p>
-           <button class="btn" type="button">LÆS MERE</button>
+          <button class="btn" type="button">LÆS MERE</button>
         </div>
-      </a>
-    `;
+      `;
+  });
+
+  //js til product view (altså når man klikker på et product)
+  //små noter
+  // henter her class'en productview fra html
+  const modal = document.querySelector(".productview");
+
+  document.querySelectorAll(".product").forEach((prod) => {
+    //“Når brugeren klikker på dette produktkort, kør denne funktion.”
+    prod.addEventListener("click", () => {
+      //vores modal finder/fylder sig med vores data vi selv har lavet i superbase
+      modal.querySelector(".product_img").src = prod.dataset.image;
+      modal.querySelector(".product_name").textContent = prod.dataset.name;
+      modal.querySelector(".product_price").textContent =
+        prod.dataset.price + " kr";
+      modal.querySelector(".product_description").textContent =
+        prod.dataset.description;
+      //Vis modal'en
+      modal.showModal();
+      modal.scrollTop = 0;
+      //Uden modal.scrollTop = 0; så vil den åbne sig i bunden, derfor har vi brug for den så den starter fra toppen af
+    });
+  });
+
+  // js til når man lukker produktet ned
+  // "Når brugeren klikker på X, så luk ned"
+  modal.querySelector(".close_btn").addEventListener("click", () => {
+    modal.close();
+  });
+
+  // Luk ved klik på
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.close();
+    }
   });
 }
